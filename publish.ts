380fe423cec5b70f1f61e71c870e87a55f81b499
Gutai Dénes <daenash@gmail.com>
2024-04-11 14:30:04 +0200

    feat: version bumper script

---
+++ publish.ts
@@ -0,0 +1,73 @@
+import { parseArgs } from "util";
+import { writeFileSync } from "fs";
+
+import rootPackageJSON from "./package.json";
+import clientPackageJSON from "./packages/client/package.json";
+import serverPackageJSON from "./packages/server/package.json";
+import { $ } from "bun";
+
+const { values } = parseArgs({
+  args: Bun.argv,
+  options: {
+    major: {
+      type: "boolean",
+      default: false,
+    },
+    minor: {
+      type: "boolean",
+      default: false,
+    },
+    patch: {
+      type: "boolean",
+      default: false,
+    },
+  },
+  strict: true,
+  allowPositionals: true,
+});
+
+const onlyOneFlag = Object.values(values).reduce<number>((pv, a) => {
+  return pv + Number(a);
+}, 0);
+
+if (onlyOneFlag !== 1) {
+  console.error("One and ONLY one version type should be set!");
+  process.exit(0);
+}
+
+let [major, minor, patch] = rootPackageJSON.version
+  .split(".")
+  .map((e) => Number(e));
+
+if (values.major) {
+  major++;
+  minor = 0;
+  patch = 0;
+}
+if (values.minor) {
+  minor++;
+  patch = 0;
+}
+if (values.patch) {
+  patch++;
+}
+
+const newVersion = [major, minor, patch].join(".");
+
+clientPackageJSON.version = newVersion;
+serverPackageJSON.version = newVersion;
+rootPackageJSON.version = newVersion;
+
+writeFileSync(
+  "./packages/client/package.json",
+  JSON.stringify(clientPackageJSON)
+);
+writeFileSync(
+  "./packages/server/package.json",
+  JSON.stringify(serverPackageJSON)
+);
+writeFileSync("./package.json", JSON.stringify(rootPackageJSON));
+
+await $`bunx prettier ./packages/client/package.json --write`;
+await $`bunx prettier ./packages/server/package.json --write`;
+await $`bunx prettier ./package.json --write`;
